5e9e3118680be1178d1ede677a9bfdcb31e0227b
turkerge <turkerge@garanti.com.tr>
2018-07-12 17:59:41 +0300

    test code

--- src/main/java/com/gt/speectotextgarantiunifiedcomm/SpeectoTextGarantiUnifiedcommApplication.java
+++ src/main/java/com/gt/speectotextgarantiunifiedcomm/SpeectoTextGarantiUnifiedcommApplication.java
@@ -1,14 +1,15 @@
 package com.gt.speectotextgarantiunifiedcomm;
 
+import com.google.cloud.speech.v1.*;
+import com.google.protobuf.ByteString;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.google.cloud.speech.v1.SpeechClient;
-import com.google.cloud.speech.v1.RecognitionAudio;
-import com.google.cloud.speech.v1.RecognitionConfig;
-import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
-import com.google.cloud.speech.v1.RecognizeResponse;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @SpringBootApplication
 public class SpeectoTextGarantiUnifiedcommApplication {
@@ -22,22 +23,40 @@ public class SpeectoTextGarantiUnifiedcommApplication {
 	private static void go() {
 
 		try (SpeechClient speechClient = SpeechClient.create()) {
+
+			Path path = Paths.get("\\\\pgarappstp01\\Share\\karisik.flac");
+			byte[] data = Files.readAllBytes(path);
+			ByteString audioBytes = ByteString.copyFrom(data);
+
 			RecognitionConfig.AudioEncoding encoding = RecognitionConfig.AudioEncoding.FLAC;
 			int sampleRateHertz = 44100;
-			String languageCode = "en-US";
+			String languageCode = "tr-TR";
 			RecognitionConfig config = RecognitionConfig.newBuilder()
 					.setEncoding(encoding)
 					.setSampleRateHertz(sampleRateHertz)
 					.setLanguageCode(languageCode)
 					.build();
-			String uri = "file:\\\\pgarappstp01\\Share\\turker.mp3";
+
 			RecognitionAudio audio = RecognitionAudio.newBuilder()
-					.setUri(uri)
+					.setContent(audioBytes)
 					.build();
 			RecognizeResponse response = speechClient.recognize(config, audio);
+
+			List<SpeechRecognitionResult> results = response.getResultsList();
+
+			System.out.println("RESULTS :");
+			for (SpeechRecognitionResult result : results) {
+				// There can be several alternative transcripts for a given chunk of speech. Just use the
+				// first (most likely) one here.
+				SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+				System.out.printf("Transcription: %s%n", alternative.getTranscript());
+			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		System.out.println("END :");
 
 	}
 }
